e36e8fa85ab794267d30f96acd071ff8a6758210
MaratAbashev <maratabashev@yandex.ru>
2025-04-12 20:59:28 +0300

    removed id from register method

--- AsyncQueue/Broker/Interfaces/IConsumerService.cs
+++ AsyncQueue/Broker/Interfaces/IConsumerService.cs
@@ -5,6 +5,6 @@ namespace Broker.Interfaces;
 public interface IConsumerService
 {
     public ResponseMessageDto Poll(Guid consumerId);
-    public bool Register(string consumerGroup, Guid consumerId);
+    public bool Register(string consumerGroup);
     public ResponseMessageDto Consume(Guid consumerId); // другая дто
 }
